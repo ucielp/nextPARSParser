c5a0730fc9cd15f98c939444781404125cbb3ceb
ucielp <ucielp@gmail.com>
2020-01-27 12:59:51 +0100

    Remove extra comments

--- nextPARSParser.py
+++ nextPARSParser.py
@@ -23,7 +23,6 @@ features = HTSeq.GenomicArrayOfSets( "auto", stranded=True )
 # TODO remove this line
 # features = HTSeq.GenomicArrayOfSets("auto", stranded != "no")
 
-# TODO, put it in an object
 features_properties = dict()
 
 
@@ -64,13 +63,11 @@ def count_reads_by_position_in_features(bam_file,gtf_file,out_file,feature_type,
 				
 				features[ f.iv ] += feature_id
 				features_properties[ f.name ] = f.iv.length,f.iv.start,f.iv.end
-				# genes = Gene(f.name,f.iv.length,f.iv.start,f.iv.end)
 		
 
 				if stranded != "no" and f.iv.strand == ".":
 					raise ValueError(
-							"Feature %s at %s does not have strand information but you are "
-							"running htseq-count in stranded mode. Use '--stranded=no'." %
+							"Feature %s at %s does not have strand information" %
 							(f.name, f.iv))
 
 				k += 1
@@ -89,24 +86,15 @@ def count_reads_by_position_in_features(bam_file,gtf_file,out_file,feature_type,
 		sys.stderr.write("%d GFF lines processed.\n" % k)
 		sys.stderr.flush()
 
-
-
-	# elapsed_time = time.process_time() - t
-	# print("ELAPSED1 : ",elapsed_time)
-	
 	# GET COUNTS
 	bam = HTSeq.BAM_Reader(bam_file)
 	counts = collections.Counter( )
 	k = 0
 	genes_set = set()
 
-
-	# t = time.process_time()
-
 	# TODO remove this line
-	# and remove itertools
-	# ~ for align in itertools.islice( bam, 100000):
-	for align in bam:
+	for align in itertools.islice( bam, 100000):
+	# ~ for align in bam:
 
 		
 		if k > 0 and k % 100000 == 0 and not quiet:
@@ -116,7 +104,7 @@ def count_reads_by_position_in_features(bam_file,gtf_file,out_file,feature_type,
 		k += 1
 		
 
-		
+		# TODO: Remove this
 		# ~ for co in align.cigar:
 			# ~ # cut long cigar strings, only the best of reads, maximum one variation
 			# ~ if co.type not in com and co.size > 0:
@@ -134,29 +122,11 @@ def count_reads_by_position_in_features(bam_file,gtf_file,out_file,feature_type,
 
 		# TODO: Remove Hack			
 		if align.iv.length > 51:
-			counts[ "__too_long" ] += 1
-			# ~ for co in align.cigar:
-				# ~ # cut long cigar strings, only the best of reads, maximum one variation
-				# ~ if co.type not in com and co.size > 0:
-					# ~ print("NO",co,co.type,co.size,co.ref_iv)
-				# ~ else:
-					# ~ print("SI",co,co.type,co.size,co.ref_iv)
+			# counts[ "_too_long" ] += 1
 			continue
 		
-		
-		# ~ iv_seq = (co.ref_iv for co in align.cigar if co.type in com
-                                  # ~ and co.size > 0)
- 
-		# ~ fs = set()
-
-		# ~ for iv in iv_seq:
-			# ~ if iv.chrom not in features.chrom_vectors:
-				# ~ raise UnknownChrom
-			# ~ for iv2, fs2 in features[iv].steps():
-				# ~ fs = fs.union(fs2)
-				
 		if align.aQual < minaqual:
-			counts[ "__too_low_aQual" ] += 1
+			counts[ "_too_low_aQual" ] += 1
 			continue
 
 		gene_ids = set()
@@ -164,10 +134,7 @@ def count_reads_by_position_in_features(bam_file,gtf_file,out_file,feature_type,
 		# interval, gen
 		for iv_f, gene in features[ align.iv ].steps():
 			# Add element to the set.
-			# ~ print ("A",iv_f,gene)
 			gene_ids |= gene
-
-		# overlap_mode (same as htseq-count union)
 
 		if len(gene_ids) == 1:
 			gene_id = list(gene_ids)[0]
@@ -189,31 +156,19 @@ def count_reads_by_position_in_features(bam_file,gtf_file,out_file,feature_type,
 			else:
 				sys.exit("Illegal multimap mode.")
 	
-	# elapsed_time = time.process_time() - t
-	# print("ELAPSED2 : ",elapsed_time)
-	
 	# GET RESULTS
-	# New dictionary sort on first element of the key and then on the second element
-	# sorted_counts = collections.OrderedDict(sorted(counts.items(),key = lambda i: (i[0][0], i[0][1])))
-	# ~ print ("Length: ",len(counts))
 	
-	# ~ for a in counts:
-		# ~ print (a)
-	
-	# New list sort on gene_name and then on nt position on that gene
+	# New dictionary sort on gene_name and then on nt position on that gene
 	sorted_counts = sorted(counts.items(),key = lambda i: (i[0][0], i[0][1]))
 
 	last_gene_id = ''
 	additional_data = []
 	list_of_reads = []
 	
-	# t0 = time.process_time()
-	# print (sorted_counts)
 	my_iter = iter(sorted_counts)
 	
-	# TODO rename this variable to number of ??
-	p = 0
 	for (index, gene_pos_count) in enumerate(sorted_counts[:-1]):
+		# print(gene_pos_count)
 		if index < len(sorted_counts):
 			current, next_ = gene_pos_count, sorted_counts[index + 1]
 			
@@ -233,24 +188,15 @@ def count_reads_by_position_in_features(bam_file,gtf_file,out_file,feature_type,
 			additional_data.append((gene_id_pos,read_counts))
 			
 		else:
-			p += 1
 			gene_id = gene_id_pos[0] 
 			position = gene_id_pos[1]
 			next_gene_id = next_[0][0]
 			
-			# TODO: Remove this
-			# gene_id_length = features_properties.get(gene_id)[0]
+			gene_id_length = features_properties.get(gene_id)[0]
 			gene_id_start = features_properties.get(gene_id)[1]
 			gene_id_end = features_properties.get(gene_id)[2]
 		
-			# print(gene_id,next_gene_id)
-
-			# elapsed_time = time.process_time() - t
-			# print("END0 : ",elapsed_time)
-			# t = time.process_time()
-
 		# Check if there is something mapped
-		# TODO check this because it should continue to print _unmapped etc
 		try: 
 			gene_id
 		except:
@@ -261,129 +207,52 @@ def count_reads_by_position_in_features(bam_file,gtf_file,out_file,feature_type,
 		if (last_gene_id != gene_id) :
 			i = gene_id_start
 			
-			# print(gene_id)
 			reads = []
-			# reads.append(gene_id+"\t")
 			reads.append(gene_id)
 			last_gene_id = gene_id
-		
-		
-			# elapsed_time = time.process_time() - t
-			# print("END1 : ",elapsed_time)
-			# t = time.process_time()
 
 		# Still working with the same gene
 		if (gene_id == next_gene_id):
 
 			while (i < position):
-				# print("pos 0: ", i, "pos ", position, "counts: ",0, end = '\n')
-				# TODO llenar con 0 al principio
+				# TODO: Extend with 0s
 				# reads.extend(repeat(0, position - i))
 				# i = position
-				# ~ print(position-i,gene_id,i,position)
 				reads.append(0)
 				i+=1
 			if i == position:
-				# print("pos A: ", i, "pos ", position, "gene_id_start",gene_id_start, "counts: ",read_counts, end = '\n')
-				# print(current)
-				# reads.append(sorted_counts[gene_id_pos])
 				reads.append(read_counts)
-				# reads.insert(position, sorted_counts[gene_id_pos])
 				i+=1
 				
 		# Last position(s) of the gene
 		else:
 			if i == position:
-				# print("pos B: ", i, "pos ", position, "counts: ", sorted_counts[tuple_e], end = '\n')
-				# reads.append(sorted_counts[gene_id_pos])
 				reads.append(read_counts)
-				# reads.insert(position, sorted_counts[gene_id_pos])
-
 				i+=1
+
 			while (i < gene_id_end):
-				# print("pos X: ", i, "pos ", position, "counts: ",0, end = '\n')
+				# TODO: Extend with 0s
+				# reads.extend(repeat(0, gene_id_end - i))
 				reads.append(0)
 				i+=1
-			# ~ else:
-				# ~ # TODO
-				# ~ # complete with 0s
-				# ~ # reads.extend(repeat(0, gene_id_end - i))
-				# ~ # i = gene_id_end
-				# ~ while (i < gene_id_end):
-					# ~ reads.append(0)
-					# ~ i+=1
-			
-			# Add reads to the final
-			#reads[0] = ''.join(str(reads[0:2]))
 
 			list_of_reads.append(reads)
+			# print(reads[0])
 		
 	# Last position of the file
 	# TODO put this one
 	gene_id_pos = next_[0]
 	read_counts = next_[1]
 	
-	# ~ if isinstance(gene_id_pos, str):
-		# ~ print("Entra?")
-		# ~ end_table.append((gene_id_pos,read_counts))
-		# ~ print(end_table)
-		
-		# ~ df = pd.DataFrame(data=end_table)
-		# ~ # Append mode
-		# ~ df.to_csv(FILE_TO_WRITE, mode='a', index=False,header=False)
-		
-	# ~ else:
-		# ~ gene_id = gene_id_pos[0] 
-		# ~ position = gene_id_pos[1]
-		# ~ next_gene_id = next_[0][0]
-		
-		# ~ # TODO: Remove this
-		# ~ # gene_id_length = features_properties.get(gene_id)[0]
-		# ~ gene_id_start = features_properties.get(gene_id)[1]
-		# ~ gene_id_end = features_properties.get(gene_id)[2]
-	
-		# ~ print(gene_id,next_gene_id)
-		
-		
-
-			# elapsed_time = time.process_time() - t
-			# print("END0 : ",elapsed_time)
-			# t = time.process_time()
-
-		# elapsed_time = time.process_time() - t
-		# print("END2 : ",elapsed_time)
-
-	# elapsed_time = time.process_time() - t0
-	# print("END0 : ",elapsed_time)	
-	
-	# TODO: Remove pandas
-	# Append mode
-	# df = pd.DataFrame(data=list_of_reads)
-	# df.to_csv(FILE_TO_WRITE, mode='a', index=False,header=False)
-	
-	# Append mode
+	# Write to file
 	write_to_file(out_file,additional_data)
 	write_to_file(out_file,list_of_reads)
-	# To test I add header 
-	# df.to_csv(FILE_TO_WRITE, index=False, mode='a')
 
-
-
-
-# Write to file
 def write_to_file (FILE_TO_WRITE,data_to_write):
 	with open(FILE_TO_WRITE,"a") as f:
 		wr = csv.writer(f,delimiter=';')
 		wr.writerows(data_to_write)
 		
-
-
-
-
-
-
-# TODO add a method to write to a file
-# write_to_samout
 
 def main():
 	pa = argparse.ArgumentParser(
